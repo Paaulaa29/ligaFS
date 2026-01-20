74d7feda39fe21c717774f957256c27f3c9cf851
Paaulaa29 <alumno.647654@ies-azarquiel.es>
2026-01-20 12:35:01 +0100

    Cuarto Commit

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+require('dotenv').config();
 
 // Initialize Firebase with your config
 const firebaseConfig = {
-  apiKey: "AIzaSyBujBSd9aC74z8yTbjbzfTLQc5_w3Ey88M",
-  authDomain: "ligaf-5a475.firebaseapp.com",
-  projectId: "ligaf-5a475",
-  storageBucket: "ligaf-5a475.firebasestorage.app",
-  messagingSenderId: "578692439472",
-  appId: "1:578692439472:web:5f0108dd0582f493738a75"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
